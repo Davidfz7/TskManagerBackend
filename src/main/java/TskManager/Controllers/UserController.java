5d96595a6e75378329bdffe8b1895a16d13e6e75
DaichiIkezawa <ikesawa.daichi@gmail.com>
2023-11-03 12:12:48 -0600

    Aqui nuevo codigo CRUD

--- src/main/java/TskManager/Controllers/UserController.java
+++ src/main/java/TskManager/Controllers/UserController.java
@@ -7,46 +7,88 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import TskManager.Tables.Users;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.persistence.PostPersist;
-import java.io.IOException;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-
+import TskManager.Tables.Users;
+import org.springframework.web.bind.annotation.CrossOrigin;
 //Esta anotacion indica que esto es un controlador en Django seria algo como la clase VIEWS
 @RestController
 //Esto indica que esta clase es una URL la cual
 //Va a contener contenido para mostrar en pantalla EN DJANGO SERIA ALGO COMO LA CLASE URL
-@RequestMapping(path = "/api") 
+//NOTA IMPORTANTE
+@RequestMapping(path = "/api/users") //NOTA: Cambie de ruta base para una mejor organización
+//NOTA IMPORTABTE
 @CrossOrigin
 public class UserController {
-     
-     private final UserService userService;
-     
-     @Autowired //Code Injection in this constructor
-     public UserController(UserService userService){
-         
-         this.userService = userService; 
-     
-     }
-     
-    @GetMapping("/users")//Aqui se indica el contenido a mostrar o devolver en esta direccion
-    public List<Users> getUser(){
     
-        return userService.getUsers();
+    private final UserService userService;
+    
+    @Autowired
+    public UserController(UserService userService){
+        this.userService = userService;
     }
     
-   @PostMapping("/postbody")
-   public String postBody(@RequestBody Users user){
-       userService.saveUser(user);
-       
-       return "Se entrego la data";
-   }
+    @GetMapping
+    public ResponseEntity<List<Users>> getUsers() {
+        List<Users> users = userService.getUsers();
+        return ResponseEntity.ok(users);
+    }
     
+    @GetMapping("/{id}")
+    public ResponseEntity<Users> getUserById(@PathVariable Long id) {
+        Users user = userService.getUserById(id);
+        if (user != null) {
+            return ResponseEntity.ok(user);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
     
+    @PostMapping
+    public ResponseEntity<Users> createUser(@RequestBody Users user) {
+        try {
+            Users createdUser = userService.createUser(user);
+            return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+        } 
+        catch (IllegalArgumentException e) {
+           //return ResponseEntity.badRequest().body(e.getMessage());
+           //Nota; a ser posible, cambiar a un ResponseEntity para un mensaje de error
+           return null;
+        }
+    }
     
+    @PutMapping("/{id}")
+    public ResponseEntity<Users> updateUser(@PathVariable Long id, @RequestBody Users updatedUser) {
+        Users user = userService.getUserById(id);
+        if (user != null) {
+            Users updatedUserResult = userService.updateUser(id, updatedUser);
+            if (updatedUserResult != null) {
+                return ResponseEntity.ok(updatedUserResult);
+            } else {
+                //return ResponseEntity.badRequest().body(new ErrorResponse("No se pudo actualizar el usuario"));
+                //Nota; a ser posible, cambiar a un ResponseEntity para un mensaje de error
+                return null;
+            }
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        Users user = userService.getUserById(id);
+        if (user != null) {
+            userService.deleteUser(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
+
